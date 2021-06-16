0f6555ddc0ed5f814b7bd81d64faf70443d92ea2
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-06-16 12:03:15 +0300

    database fully connected

--- JS-BackEnd-Node.js/MongoDB/workshop/services/storage.js
+++ JS-BackEnd-Node.js/MongoDB/workshop/services/storage.js
@@ -1,31 +1,6 @@
 const Cube = require('../models/Cube');
-const fs = require('fs/promises')
-const uniqid = require('uniqid')
-
-// load and parse data file
-// provide ability to:
-// - read all entries
-// - read single entry
-// - create entry
-// * - search
-
-let data = {}
-
-// model structure
-// "test1": {
-// "name": "test1-name",
-// "description": "item",
-// "image URL": "none",
-// "difficulty": "test"
-// }
 
 async function init() {
-    try {
-        data = JSON.parse(await fs.readFile('./models/data.json'));
-    } catch (err) {
-        console.error("Error reading database");
-    }
-    
     return (req, res, next) => {
         req.storage = {
             getAll,
@@ -37,45 +12,36 @@ async function init() {
 }
 
 async function getAll(query) {
-    let cubes = Object
-    .entries(data)
-    .map(([id, v]) => Object.assign({}, { id }, v));
-
+    const options = {}
+    
     if (query.search) {
-        cubes = cubes.filter(c => c.name.toLowerCase().includes(query.search.toLowerCase()));
+       options.name = { $regex: query.search, $options: 'i' };
     }
     if (query.from) {
-        cubes = cubes.filter((c) => Number(c.difficulty) >= Number(query.from));
+        options.difficulty = { $gte: Number(query.from) };
     }
     if (query.to) {
-        cubes = cubes.filter((c) => Number(c.difficulty) <= Number(query.to));
+        options.difficulty = options.difficulty || {};
+        options.difficulty.$lte = Number(query.to);
     }
-
+    console.log(options);
+    const cubes = Cube.find(options).lean();
     return cubes;
 }
 
 async function getById(id) {
-    return data[id]
+    const cube = await Cube.findById(id).lean();
+    if (cube) {
+        return cube;
+    }else {
+        return undefined;
+    }
 }
 
 async function create(cube) {
     const record = new Cube(cube);
     return record.save();
 }
-
-// async function search(str, minD, maxD) {
-//     // vikash getALL
-//     // filter po min i max
-//     //filter po ime 
-
-//     const cubes  = await getAll();
-//     const result = cubes
-//     //   .filter((i) => Number(i.difficulty) >= minD)
-//       .filter((x) => Number(x.difficulty) <= maxD);
-//     console.log('test');
-    
-//     return cubes
-// }
 
 module.exports = {
     init,
